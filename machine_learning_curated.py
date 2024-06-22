0e1f9365d9887654827edd571bf9f1cc896a8a28
ScottTpirate <44659733+ScottTpirate@users.noreply.github.com>
2024-06-22 15:31:17 -0500

    fixing type issue

--- machine_learning_curated.py
+++ machine_learning_curated.py
@@ -32,12 +32,12 @@ SqlQuery0 = '''
 SELECT 
     s.sensorReadingTime,
     s.serialNumber as stepTrainerSerialNumber,
-    s.distanceFromObject,
+    CAST(s.distanceFromObject AS FLOAT) as distanceFromObject, -- casting to FLOAT if necessary
     a.user as email,
     a.timeStamp as accelerometerTimeStamp,
-    a.x,
-    a.y,
-    a.z
+    CAST(a.x AS FLOAT) as x,  -- ensure this matches the FLOAT type in table definition
+    CAST(a.y AS FLOAT) as y,
+    CAST(a.z AS FLOAT) as z
 FROM 
     step_trainer_trusted s
 JOIN 
@@ -48,6 +48,6 @@ JOIN
 SQLQuery_node1719086409736 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"step_trainer_trusted":step_trainer_trusted_node1719086371042, "accelerometer_trusted":accelerometer_trusted_node1719086370653, "customers_curated":customers_curated_node1719086370042}, transformation_ctx = "SQLQuery_node1719086409736")
 
 # Script generated for node ml_curated
-ml_curated_node1719086455134 = glueContext.write_dynamic_frame.from_catalog(frame=SQLQuery_node1719086409736, database="stedi", table_name="machine_learning_curated", transformation_ctx="ml_curated_node1719086455134")
+ml_curated_node1719086455134 = glueContext.write_dynamic_frame.from_catalog(frame=SQLQuery_node1719086409736, database="stedi", table_name="machine_learning_curated", additional_options={"enableUpdateCatalog": True, "updateBehavior": "UPDATE_IN_DATABASE"}, transformation_ctx="ml_curated_node1719086455134")
 
 job.commit()
